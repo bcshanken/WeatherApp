b6e4fd558d189461532eef00bbb67b93e4addc29
Brian Shanken <bcshanken@gmail.com>
2020-10-11 17:22:56 -0400

    list of cities generates usign jquery, not in local memory yet. the active city display now has a border and spacing

--- Assets/Java/script.js
+++ Assets/Java/script.js
@@ -2,19 +2,34 @@ $(document).ready(function() {
     console.log("connected")
     // Dom Variables
     var newCity = "";
+    var cityList=["Philly", "San Fran"];
     
     
-    
-    
+    function renderCityList (){
+        $("#cityHistory").empty();    
+        for(i=0;i<cityList.length;i++){
+            console.log(cityList[i]);
+            var newLiEl = $("<div>");
+            newLiEl.attr("id",cityList[i]);
+            newLiEl.attr("class","historyEl");
+            newLiEl.text(cityList[i]);
+            $("#cityHistory").append(newLiEl);
+        }
+    }
     
     
     // Event Listeners
     $("#cityButton").on("click", function(event) {
         event.preventDefault();
-        console.log($(this).parent().parent().parent().parent().find("input").val());
+        newCity= ($(this).parent().parent().parent().parent().find("input").val());
         // newCity = document.getElementById("cityInput");
-        // console.log(newCity);
-        
+        console.log("the new city is " + newCity);
+        cityList.push(newCity);
+        console.log(cityList);
+        renderCityList();
       });
+
+        //Function calls
+        renderCityList();
 
   });
