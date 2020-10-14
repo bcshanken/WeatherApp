e1e328b8f983da94db2637227e5ec27a4ac767cc
Brian Shanken <bcshanken@gmail.com>
2020-10-13 21:12:18 -0400

    styling buttons under city history for event delegation

--- Assets/Java/script.js
+++ Assets/Java/script.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
   console.log("connected");
   // Dom Variables
   var newCity = "";
-  var cityList = ["Philly", "San Fran"];
+  var cityList = JSON.parse(localStorage.getItem("savedCities"));
+  console.log(cityList);
   var selectedCity = "";
   var selectedCityTemp = "";
   var selectedCityHumidity = "";
@@ -22,9 +23,10 @@ $(document).ready(function () {
     $("#cityHistory").empty();
     for (i = 0; i < cityList.length; i++) {
       console.log(cityList[i]);
-      var newLiEl = $("<div>");
-      newLiEl.attr("id", cityList[i]);
+      var newLiEl = $("<button>");
+      newLiEl.attr("id", "savedCity");
       newLiEl.attr("class", "historyEl");
+      newLiEl.attr("data-local", cityList[i]);
       newLiEl.text(cityList[i]);
       $("#cityHistory").append(newLiEl);
     }
@@ -72,34 +74,51 @@ $(document).ready(function () {
     $("#dayThreeDate").text(futureThree);
     $("#dayFourDate").text(futureFour);
     $("#dayFiveDate").text(futureFive);
-}
-    function forcastData(){
-        var queryURL =
+  }
+  function forcastData() {
+    var queryURL =
       "https://api.openweathermap.org/data/2.5/forecast?q=" +
       selectedCity +
       "&units=imperial&appid=65a8340c47c8039a8355bd288e7f9d5a";
-      
-      $.ajax({
-          url: queryURL,
-          method: "GET",
-      }).then(function (responseTwo){
 
-            // forcast temp
-          $("#dayOneTemp").text("Temp: " + Math.floor(responseTwo.list[0].main.temp_max) + " F");
-          $("#dayTwoTemp").text("Temp: " + Math.floor(responseTwo.list[8].main.temp_max) + " F");
-          $("#dayThreeTemp").text("Temp: " + Math.floor(responseTwo.list[16].main.temp_max) + " F");
-          $("#dayFourTemp").text("Temp: " + Math.floor(responseTwo.list[24].main.temp_max) + " F");
-          $("#dayFiveTemp").text("Temp: " + Math.floor(responseTwo.list[32].main.temp_max) + " F");
-            // forcast humidity
-            $("#dayOneHumidity").text("Humidity: " + Math.floor(responseTwo.list[0].main.humidity) + " %");
-          $("#dayTwoHumidity").text("Humidity: " + Math.floor(responseTwo.list[8].main.humidity) + " %");
-          $("#dayThreeHumidity").text("Humidity: " + Math.floor(responseTwo.list[16].main.humidity) + " %");
-          $("#dayFourHumidity").text("Humidity: " + Math.floor(responseTwo.list[24].main.humidity) + " %");
-          $("#dayFiveHumidity").text("Humidity: " + Math.floor(responseTwo.list[32].main.humidity) + " %");
-      })
-    }
-    
- 
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (responseTwo) {
+      // forecast temp
+      $("#dayOneTemp").text(
+        "Temp: " + Math.floor(responseTwo.list[0].main.temp_max) + " F"
+      );
+      $("#dayTwoTemp").text(
+        "Temp: " + Math.floor(responseTwo.list[8].main.temp_max) + " F"
+      );
+      $("#dayThreeTemp").text(
+        "Temp: " + Math.floor(responseTwo.list[16].main.temp_max) + " F"
+      );
+      $("#dayFourTemp").text(
+        "Temp: " + Math.floor(responseTwo.list[24].main.temp_max) + " F"
+      );
+      $("#dayFiveTemp").text(
+        "Temp: " + Math.floor(responseTwo.list[32].main.temp_max) + " F"
+      );
+      // forecast humidity
+      $("#dayOneHumidity").text(
+        "Humidity: " + Math.floor(responseTwo.list[0].main.humidity) + " %"
+      );
+      $("#dayTwoHumidity").text(
+        "Humidity: " + Math.floor(responseTwo.list[8].main.humidity) + " %"
+      );
+      $("#dayThreeHumidity").text(
+        "Humidity: " + Math.floor(responseTwo.list[16].main.humidity) + " %"
+      );
+      $("#dayFourHumidity").text(
+        "Humidity: " + Math.floor(responseTwo.list[24].main.humidity) + " %"
+      );
+      $("#dayFiveHumidity").text(
+        "Humidity: " + Math.floor(responseTwo.list[32].main.humidity) + " %"
+      );
+    });
+  }
 
   // Event Listeners
   $("#cityButton").on("click", function (event) {
@@ -114,6 +133,11 @@ $(document).ready(function () {
     localStorage.setItem("savedCities", JSON.stringify(cityList));
     getCurrentData();
     forcastData();
+  });
+
+  // Selecting from the saved Cities
+  $("#savedCity").on("click", function (event) {
+    console.log("history button clicked");
   });
 
   //Function calls
